a010f49766193c22a521717bbfd0dda4616198a8
ablashika <kofoworola203@gmailcom>
2023-01-15 00:36:00 +0000

    harmlessRansomeAlgo

---
+++ algoritms/harmlessRansomeCode.js
@@ -0,0 +1,39 @@
+function harmlessRansomeNote (noteText, magazineText) {
+
+   let noteArray = noteText.split(' ')
+   let magazineArray = magazineText.split(' ')
+   let magazineObj = { }
+   let foundAllNoteText = true;
+
+   magazineArray.forEach(word => {
+    if (!magazineObj[word]) {
+        magazineObj[word] = 0;
+        magazineObj[word]++;
+    };
+});
+    
+    //finding if notetext is in magazi obg
+     
+       noteArray.forEach(word => {
+           if (magazineObj[word]) {
+               magazineObj[word]--;
+                 if (magazineObj[word] < 0)
+                  {
+                    foundAllNoteText = false;
+                  }
+           } 
+           else 
+           {
+            foundAllNoteText = false;
+           }
+
+          })
+
+   console.log(foundAllNoteText)
+   
+
+return foundAllNoteText  
+}
+
+
+console.log(harmlessRansomeNote("make a call", "make a call here and text there for jj"))
