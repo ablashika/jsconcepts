5d3ac31f8cfd2c81beec6507b16e997b0e447c64
ablashika <kofoworola203@gmail.com>
2023-09-13 14:29:10 +0000

    array and an oneject

---
+++ objects&Arrays.js
@@ -0,0 +1,15 @@
+//difference between an array and an oneject and relationship
+const tekkenCharacter = {
+    player: 'Hwoarang',
+    fightingStyle: 'Tae Kwon Doe',
+    human: true
+  };
+  
+  
+  let eyescolor = "eyes";
+  
+  tekkenCharacter[eyescolor] = "black"
+  console.log(tekkenCharacter)
+  
+  console.log(tekkenCharacter[eyes])
+  
