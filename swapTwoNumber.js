21266f773796ab3ee1994c378ca14fc944cff557
ablashika <kofoworola203@gmail.com>
2023-09-27 13:25:38 +0000

    swap two numbers

---
+++ swapTwoNumber.js
@@ -0,0 +1,46 @@
+function swapTwoNumbers(a,b){
+     let arr = [b, a]
+     let  [swappedA, swappedB] = arr
+     console.log(swappedA, swappedB)
+     return arr
+}
+
+
+console.log(swapTwoNumbers(3,7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    let arr = []
+    //     arr.push(a,b)
+    //    let result =[]
+    //    let first = arr[0];
+    //    console.log(first)
+    //   let last = arr[arr.length-1]
+    //   console.log(last)
+
+
+    // let arr = [a, b];
+    // let first = arr[0];
+    // let last = arr[arr.length - 1];
+    // arr[0] = last;
+    // arr[arr.length - 1] = first;
+    // return arr;  
